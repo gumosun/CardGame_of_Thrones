3449ab576af0e8d71b00985b7131ce9a3a11a47c
Hao Sun <hao.sun@nyu.edu>
2017-07-13 12:45:00 -0400

    input sound

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+// Create the cards
+function card(family, name, power, def) {
+  this.family = family;
+  this.name = name;
+  this.power = power;
+  this.def = def;
+}
+
+var jonSnow = new card("stark", "Jon Snow", 80, 75);
+var aryaStark = new card("stark", "Arya Stark", 85, 40);
+var sansaStark = new card("stark", "Sansa Stark", 50, 40);
+var cerseiLannister = new card("lannister", "Cersei Lannister", 40, 70);
+var jaimeLannister = new card("lannister", "Jaime Lannister", 75, 85);
+var tyrionLannister = new card("lannister", "Tyrion Lannister", 50, 50);
+
+var player = 0;
+
 // To load and start the game
-
-
 $('#startButton').click(function() {
   $(this).remove();
   $('#mainTitle').text('Please Select Your Family');
@@ -9,30 +24,54 @@ $('#startButton').click(function() {
                        'font-size': "72px",
                        'animation': "fadeInDown 1s linear"});
   $('<div class="familyDiv" id="houseStark">').appendTo('.mainDiv');
-  $('<div class="familyDiv" id="houseLanister">').appendTo('.mainDiv');
+  $('<div class="familyDiv" id="houseLannister">').appendTo('.mainDiv');
   $('.familyDiv').css('animation', "fadeIn 3s linear");
   playerChoice();
-
 });
 
 
 function playerChoice() {
-var player = 0;
 $('#houseStark').click(function() {
   $('.familyDiv').remove();
   $('#mainTitle').remove();
-  $('body').css('background-image', "url(./images/map.jpg)");
+  $('.mainDiv').remove();
+  $('body').css({'background-image': "url(./images/map.jpg)", 'background-size': "cover"});
   player = 1;
-  return player;
+  deployCards(player);
 });
-$('#houseLanister').click(function() {
+$('#houseLannister').click(function() {
   $('.familyDiv').remove();
   $('#mainTitle').remove();
+  $('.mainDiv').remove();
   $('body').css('background-image', "url(./images/map.jpg)");
   player = 2;
-  return player;
+  deployCards(player);
 });
-  console.log(player);
+
+}
+
+function deployCards(player){
+  $('<div id="row1">').appendTo('container');
+  $('<div id="row2">').appendTo('container');
+    for (let i=0 ; i<3; i++) {
+    $(`<div id="${i}">`).appendTo('#row1');
+    }
+    for (let i=0 ; i<3; i++) {
+    $(`<div id="${i}">`).appendTo('#row2');
+    }
+   if (player == 1) {
+      $('#row2').addClass('houseStark');
+      $('#row1').addClass('houseLannister');
+   } else {
+      $('#row1').addClass('houseStark');
+      $('#row2').addClass('houseLannister');
+   }
+
+  // if (player == 1){
+
+  // } else {
+
+  // }
 }
 
 
